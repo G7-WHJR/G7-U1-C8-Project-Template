8b7a80ee5ccb4f323b6e0510cff3f26415e16d61
unknown <raveen.anand@whitehatjr.com>
2021-10-27 21:03:23 +0530

    v2

--- Bouncing Balls/sketch.js
+++ Bouncing Balls/sketch.js
@@ -15,39 +15,40 @@ function setup() {
   //Top Edge
   topEdge = createSprite(225, 0, 450, 25);
   topEdge.shapeColor = "gray";
-  topEdge.immovable = true;
 
   //Bottom Edge
   bottomEdge = createSprite(225, 400, 450, 25);
   bottomEdge.shapeColor = "gray";
-  bottomEdge.immovable = true;
 
   //Left Edge
   leftEdge = createSprite(0, 200, 25, 400);
   leftEdge.shapeColor = "gray";
-  leftEdge.immovable = true;
 
   //Right Edge
   rightEdge = createSprite(450, 200, 25, 400);
   rightEdge.shapeColor = "gray";
-  rightEdge.immovable = true;
 }
 
 function draw() {
   background("lightyellow");
 
-  ball1.bounce(leftEdge);
-  ball1.bounce(rightEdge);
-  ball1.bounce(bottomEdge);
-  ball1.bounce(topEdge);
+  ball1.bounceOff(leftEdge);
+  ball1.bounceOff(rightEdge);
+  ball1.bounceOff(bottomEdge);
+  ball1.bounceOff(topEdge);
+
+  //ball2 bounceOff:
+  
+  
 
   if (mouseIsPressed) {
     //ball1
-    ball1.velocity.x = 1; // Left or Right
-    ball1.velocity.y = 1; // Top to Bottom
-    ball1.setSpeed(4);
+    ball1.velocityX = 6; 
+    ball1.velocityY = 6;
 
-    //ball2 velocity & Speed:
+    //ball2 velocity :
+    
+
   }
 
   drawSprites();
